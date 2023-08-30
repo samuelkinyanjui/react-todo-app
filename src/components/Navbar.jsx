2482b532ef6084c2831dd98b57f5c5972d8ffd75
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-08-30 15:00:13 +0300

    Created the inital set up of gitflow
    Did the initial setup of git flow and react App

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+const Navbar = () => {
+    const [dropdown, setDropdown] = useState(false);
+    return (
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>
+            <button>
+              Services <span>&#8595;</span>
+            </button>
+            <ul>
+              <li>Design</li>
+              <li>Development</li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+  export default Navbar;
---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,16 @@
+import Header from './Header';
+import TodosLogic from './TodosLogic';
+import Navbar from './Navbar';
+
+
+const TodoApp = () => {
+  return (
+    <>
+      <Header />
+      <TodosLogic />
+      <Navbar/ >
+
+    </>
+  );
+};
+export default TodoApp;
